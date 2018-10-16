1e17cfc769b13bd89d7d1aada55419b46b89386e
Dan Goes <dgoes@idmod.org>
2018-10-16 15:30:33 -0700

    Now supporting json file collections

--- keyToValueReplacer.py
+++ keyToValueReplacer.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import jsontools as jt
+import sys
 
 
 def replace_chunk(data):
@@ -38,9 +39,28 @@ def replace_chunk_in_file(file_path):
         json.dump(file_data, f, sort_keys=True, indent=4, separators=(', ', ': '))
 
 
+def replace_chunks_in_collection(file_path):
+    files = []
+    # Read in the collection
+    with open(file_path, "r") as file:
+        for line in file:
+            files.append(line.strip())
+
+    # Replace chunks in every file listed
+    for file in files:
+        replace_chunk_in_file(file)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="Path to the JSON file for replacement")
+    parser.add_argument("--file_path", help="Path to the JSON file for replacement")
+    parser.add_argument("--collection_path", help="Path to the file pointing to JSON files for replacement")
     args = parser.parse_args()
 
-    replace_chunk_in_file(args.file_path)
+    if args.file_path and args.collection_path or not args.file_path and not args.collection_path:
+        print("Exactly one of file_path or collection_path must be specified")
+        sys.exit(2)
+    if args.file_path:
+        replace_chunk_in_file(args.file_path)
+    elif args.collection_path:
+        replace_chunks_in_collection(args.collection_path)
